7475286fdd802d2074e7b585959a9c86637ef6ba
Ashutosh S Maakam <ashutoshsmaakam@gmail.com>
2026-02-22 12:45:43 +0530

    Update timestamp_counter.v
    Implemented parameterised timestamp counter module

--- rtl/timestamp_counter.v
+++ rtl/timestamp_counter.v
@@ -1,1 +1,17 @@
+module timestamp_counter #(
+    parameter WIDTH = 32
+)(
+    input wire clk,
+    input wire rst,
+    input wire enable,
+    output reg [WIDTH-1:0] timestamp
+);
 
+always @(posedge clk) begin
+    if (rst)
+        timestamp <= 0;
+    else if (enable)
+        timestamp <= timestamp + 1;
+end
+
+endmodule
